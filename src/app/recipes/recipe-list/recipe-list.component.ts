90b94ba921100afb45fe92768010445b8ba626dd
unknown <istvan.erdos86@gmail.com>
2019-10-02 22:24:24 +0200

    Add basic recipe list elements

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'Sample description', 'http://www.fnstatic.co.uk/images/content/recipe/fried-chicken_1.jpg'),
+    new Recipe('A test recipe', 'Sample description', 'http://www.fnstatic.co.uk/images/content/recipe/fried-chicken_1.jpg'),
+  ];
 
   constructor() { }
 
