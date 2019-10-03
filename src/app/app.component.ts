8c9284880c9e0d6489f99accd47566781dc6cad6
moon42_istvanerdos <istvan.erdos@moon42.com>
2019-10-03 16:36:15 +0200

    Add recipe item and ingredient comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  loadedFeature = 'recipes';
+
+  onNavigate(feature: string) {
+    console.log('f:' + feature);
+    this.loadedFeature = feature;
+  }
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,27 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() selectRecipe = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'Sample description', 'http://www.fnstatic.co.uk/images/content/recipe/fried-chicken_1.jpg'),
-    new Recipe('A test recipe', 'Sample description', 'http://www.fnstatic.co.uk/images/content/recipe/fried-chicken_1.jpg'),
+    new Recipe(
+      'A test recipe',
+      'Sample description',
+      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190731-air-fryer-pizza-0188-portrait-pf-1565820597.jpg'
+    ),
+    new Recipe(
+      'Another test recipe',
+      'Sample description',
+      'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2019/09/aubergine-chickpea-stew.jpg'
+    ),
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.selectRecipe.emit(recipe);
   }
 
 }
