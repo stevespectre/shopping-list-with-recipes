8505c315cb13ccfc6118b659e37402ff5ef409de
moon42_istvanerdos <istvan.erdos@moon42.com>
2019-10-16 12:57:14 +0200

    Add http to fetch and store data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -10,28 +10,35 @@ import { Subject } from 'rxjs';
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'Sample description',
-      'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190731-air-fryer-pizza-0188-portrait-pf-1565820597.jpg',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]
-    ),
-    new Recipe(
-      'Big Fat Burger',
-      'Sample description',
-      'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2019/09/aubergine-chickpea-stew.jpg',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Tasty Schnitzel',
+  //     'Sample description',
+  //     'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190731-air-fryer-pizza-0188-portrait-pf-1565820597.jpg',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Big Fat Burger',
+  //     'Sample description',
+  //     'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe/recipe-image/2019/09/aubergine-chickpea-stew.jpg',
+  //     [
+  //       new Ingredient('Buns', 2),
+  //       new Ingredient('Meat', 1)
+  //     ]
+  //   ),
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private shoppingListService: ShoppingListService) {}
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipe(index: number) {
     return this.recipes[index];
---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { RecipeService } from '../recipes/recipe.service';
+import { Recipe } from '../recipes/recipe.model';
+import { Subject } from 'rxjs';
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+  recipes: Recipe[] = [];
+  recipesChanged = new Subject<Recipe[]>();
+
+  constructor(
+    private http: HttpClient,
+    private recipeService: RecipeService
+  ) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http.put(
+      'https://ng-course-recipes-9d033.firebaseio.com/recipes.json',
+      recipes
+    )
+    .subscribe(response => console.log(response));
+  }
+
+  fetchRecipes() {
+    this.http.get<Recipe[]>('https://ng-course-recipes-9d033.firebaseio.com/recipes.json')
+    .pipe(map(recipes => {
+      return recipes.map(recipe => {
+        return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
+      });
+    }))
+    .subscribe(
+      (recipes) => {
+        this.recipeService.setRecipes(recipes);
+      }
+    );
+  }
+}
