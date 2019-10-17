243a718e374513864efea8061703f4d670cbe7e6
moon42_istvanerdos <istvan.erdos@moon42.com>
2019-10-17 10:24:43 +0200

    Add login to auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
-import { throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError, Subject } from 'rxjs';
+import { User } from './user.model';
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
+  user = new Subject<User>();
+
   constructor(private http: HttpClient) {}
 
   signup(email: string, password: string) {
@@ -24,16 +28,57 @@ export class AuthService {
         password,
         returnSecureToken: true
       }
-    ).pipe(catchError(err => {
-      let errorMessage = 'An error occured';
-      if (!err.error || !err.error.error) {
-        return throwError(errorMessage);
+    ).pipe(
+      catchError(this.handleError),
+      tap(resData => {
+        this.handleAuth(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+      })
+    );
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCsjlQWNlG3ae6Wp--a_7ORauEakkPzkcI',
+      {
+        email,
+        password,
+        returnSecureToken: true
       }
-      switch (err.error.error.message) {
-        case 'EMAIL_EXISTS':
-          errorMessage = 'This email exists already!';
-      }
+    ).pipe(
+      catchError(this.handleError),
+      tap(resData => {
+        this.handleAuth(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+      })
+    );
+  }
+
+  private handleAuth(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(
+      email,
+      userId,
+      token,
+      expirationDate
+    );
+    this.user.next(user);
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An error occured';
+    if (!errorRes.error || !errorRes.error.error) {
       return throwError(errorMessage);
-    }));
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already!';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist!';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct!';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
---
+++ src/app/auth/user.model.ts
@@ -0,0 +1,15 @@
+export class User {
+  constructor(
+    public email: string,
+    public id: string,
+    private token: string,
+    private tokenExpirationDate: Date,
+    ) {}
+
+  get userToken() {
+    if (!this.tokenExpirationDate || new Date() > this.tokenExpirationDate) {
+      return null;
+    }
+    return this.token;
+  }
+}
