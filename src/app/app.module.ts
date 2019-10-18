d94594035349bbaca919c7a332d75fd1b13e9113
moon42_istvanerdos <istvan.erdos@moon42.com>
2019-10-18 09:51:20 +0200

    Add alert component and close it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,9 +18,11 @@ import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component
 import { AuthComponent } from './auth/auth.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
 
 @NgModule({
   declarations: [
+    AlertComponent,
     AppComponent,
     AuthComponent,
     HeaderComponent,
